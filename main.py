a03a1fc45968b6f2a9803be8d6363ba45b6e7cb1
Brenton Forwood <forwoodb77@gmail.com>
2026-05-03 04:35:19 -0400

    Connected to nextjs api

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+# class Player(BaseModel):
+#   Position: str
+#   Name: str
+#   ID: str
+#   Salary: str
+#   Game_Info: str
+#   # "Game Info": str
+#   TeamAbbrev: str
+#   AvgPointsPerGame: str
 
-@app.get('/')
-def read_root():
-  return {'Hello': 'World'}
+# @app.post('/optimize')
+# def optimize(body: List[Player]):
+#   return body
+
+@app.post('/optimize')
+async def optimize(body: Request):
+  data = await body.json()
+  print(data)
+  return data
+
+# # Server
+# @app.get('/')
+# def read_root():
+#   # print('hello world')
+#   return {'Hello': 'World'}
